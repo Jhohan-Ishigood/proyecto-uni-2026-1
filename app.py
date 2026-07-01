87cb9cbde4c0251e420389d59e2d5e0991b6a4a4
Jhohan <jhohan@example.com>
2026-07-01 03:18:37 -0500

    Aquí pones una descripción breve de lo que hiciste

--- app.py
+++ app.py
@@ -2145,7 +2145,7 @@ elif not es_admin_autenticado or (es_admin_autenticado and st.session_state.rol_
                     is_fav = prod in st.session_state.favoritos
                     st.markdown(f"""
                         <div style="position:relative;">
-                            <img src="{escapar_html(src_imagen_plato)}" style="width:100%; height:200px; object-fit:cover; border-radius:12px 12px 0px 0px; box-shadow: 0px 4px 12px rgba(0,0,0,0.6); display:block; margin:0; padding:0;">
+                            <img src="{escapar_html(src_imagen_plato)}" style="width:100%; height:auto; max-height:280px; object-fit:contain; border-radius:12px 12px 0px 0px; box-shadow: 0px 4px 12px rgba(0,0,0,0.6); display:block; margin:0; padding:0; background:#1a1a1a;">
                         </div>
                     """, unsafe_allow_html=True)
                     
